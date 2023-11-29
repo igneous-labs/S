8134acd6add22bfca907ba58b7d384aaaa65c084
Jesse Y. Cho <jessecho@cs.washington.edu>
2023-11-29 20:56:26 +0900

    impl add_lst, remove_lst

--- libs/pricing-programs/flat-fee-lib/src/account_resolvers/add_lst.rs
+++ libs/pricing-programs/flat-fee-lib/src/account_resolvers/add_lst.rs
@@ -1,1 +1,28 @@
+use flat_fee_interface::{AddLstKeys, FlatFeeError, ProgramState};
+use solana_program::{pubkey::Pubkey, system_program};
+use solana_readonly_account::{KeyedAccount, ReadonlyAccountData};
 
+use crate::{pda::FeeAccountFindPdaArgs, utils::try_program_state};
+
+pub struct AddLstFreeArgs<S: KeyedAccount + ReadonlyAccountData> {
+    pub payer: Pubkey,
+    pub state: S,
+    pub lst: Pubkey,
+}
+
+impl<S: KeyedAccount + ReadonlyAccountData> AddLstFreeArgs<S> {
+    pub fn resolve(&self) -> Result<AddLstKeys, FlatFeeError> {
+        let bytes = &self.state.data();
+        let state: &ProgramState = try_program_state(bytes)?;
+
+        let find_pda_args = FeeAccountFindPdaArgs { lst: self.lst };
+        let (fee_acc, _bump) = find_pda_args.get_fee_account_address_and_bump_seed();
+
+        Ok(AddLstKeys {
+            manager: state.manager,
+            payer: self.payer,
+            fee_acc,
+            system_program: system_program::ID,
+        })
+    }
+}
--- libs/pricing-programs/flat-fee-lib/src/account_resolvers/remove_lst.rs
+++ libs/pricing-programs/flat-fee-lib/src/account_resolvers/remove_lst.rs
@@ -1,1 +1,28 @@
+use flat_fee_interface::{FlatFeeError, ProgramState, RemoveLstKeys};
+use solana_program::{pubkey::Pubkey, system_program};
+use solana_readonly_account::{KeyedAccount, ReadonlyAccountData};
 
+use crate::{pda::FeeAccountFindPdaArgs, utils::try_program_state};
+
+pub struct RemoveLstFreeArgs<S: KeyedAccount + ReadonlyAccountData> {
+    pub refund_rent_to: Pubkey,
+    pub state: S,
+    pub lst: Pubkey,
+}
+
+impl<S: KeyedAccount + ReadonlyAccountData> RemoveLstFreeArgs<S> {
+    pub fn resolve(&self) -> Result<RemoveLstKeys, FlatFeeError> {
+        let bytes = &self.state.data();
+        let state: &ProgramState = try_program_state(bytes)?;
+
+        let find_pda_args = FeeAccountFindPdaArgs { lst: self.lst };
+        let (fee_acc, _bump) = find_pda_args.get_fee_account_address_and_bump_seed();
+
+        Ok(RemoveLstKeys {
+            manager: state.manager,
+            refund_rent_to: self.refund_rent_to,
+            fee_acc,
+            system_program: system_program::ID,
+        })
+    }
+}
