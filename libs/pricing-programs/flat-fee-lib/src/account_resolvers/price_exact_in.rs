d97393bb6949b448267bb38c5199c7dd19ab39c0
Jesse Y. Cho <jessecho@cs.washington.edu>
2023-11-30 15:25:25 +0900

    add WithBump kinds for exact in/out account resolvers

--- libs/pricing-programs/flat-fee-lib/src/account_resolvers/price_exact_in.rs
+++ libs/pricing-programs/flat-fee-lib/src/account_resolvers/price_exact_in.rs
@@ -1,7 +1,7 @@
 use flat_fee_interface::PriceExactInKeys;
-use solana_program::pubkey::Pubkey;
+use solana_program::pubkey::{Pubkey, PubkeyError};
 
-use crate::pda::FeeAccountFindPdaArgs;
+use crate::pda::{FeeAccountCreatePdaArgs, FeeAccountFindPdaArgs};
 
 pub struct PriceExactInFreeArgs {
     pub input_lst_mint: Pubkey,
@@ -26,5 +26,38 @@ impl PriceExactInFreeArgs {
             input_fee_acc,
             output_fee_acc,
         }
+    }
+}
+
+pub struct PriceExactInWithBumpFreeArgs {
+    pub orig_args: PriceExactInFreeArgs,
+    pub input_fee_acc_bump: u8,
+    pub output_fee_acc_bump: u8,
+}
+
+impl PriceExactInWithBumpFreeArgs {
+    pub fn resolve(self) -> Result<PriceExactInKeys, PubkeyError> {
+        let input_create_pda_args = FeeAccountCreatePdaArgs {
+            find: FeeAccountFindPdaArgs {
+                lst_mint: self.orig_args.input_lst_mint,
+            },
+            bump: [self.input_fee_acc_bump],
+        };
+        let input_fee_acc = input_create_pda_args.get_fee_account_address()?;
+
+        let output_create_pda_args = FeeAccountCreatePdaArgs {
+            find: FeeAccountFindPdaArgs {
+                lst_mint: self.orig_args.output_lst_mint,
+            },
+            bump: [self.output_fee_acc_bump],
+        };
+        let output_fee_acc = output_create_pda_args.get_fee_account_address()?;
+
+        Ok(PriceExactInKeys {
+            input_lst_mint: self.orig_args.input_lst_mint,
+            output_lst_mint: self.orig_args.output_lst_mint,
+            input_fee_acc,
+            output_fee_acc,
+        })
     }
 }
