4db20dc57159cc1842dcc38be3b71f555f2b2789
Jesse Y. Cho <jessecho@cs.washington.edu>
2023-11-30 18:54:51 +0900

    impl SetManager ix

--- programs/pricing-programs/flat-fee/src/processor/set_manager.rs
+++ programs/pricing-programs/flat-fee/src/processor/set_manager.rs
@@ -1,5 +1,7 @@
+use flat_fee_lib::processor::{process_set_manager_unchecked, verify_set_manager};
 use solana_program::{account_info::AccountInfo, entrypoint::ProgramResult};
 
-pub fn process_set_manager(_accounts: &[AccountInfo]) -> ProgramResult {
-    todo!()
+pub fn process_set_manager(accounts: &[AccountInfo]) -> ProgramResult {
+    let checked = verify_set_manager(accounts)?;
+    process_set_manager_unchecked(checked)
 }
