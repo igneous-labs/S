9f79cca5a772252bbd1d238bda47e989fb8205d1
Jesse Y. Cho <jessecho@cs.washington.edu>
2023-12-01 14:56:28 +0900

    impl program processor::{initialize,set_lp_withdrawal_fee}

--- programs/pricing-programs/flat-fee/src/processor/initialize.rs
+++ programs/pricing-programs/flat-fee/src/processor/initialize.rs
@@ -1,5 +1,14 @@
+use flat_fee_lib::{
+    initial_constants,
+    processor::{process_initialize_unchecked, verify_initialize},
+};
 use solana_program::{account_info::AccountInfo, entrypoint::ProgramResult};
 
-pub fn process_initialize(_accounts: &[AccountInfo]) -> ProgramResult {
-    todo!()
+pub fn process_initialize(accounts: &[AccountInfo]) -> ProgramResult {
+    let checked = verify_initialize(accounts)?;
+    process_initialize_unchecked(
+        checked,
+        initial_constants::initial_manager::ID,
+        initial_constants::INITIAL_LP_WITHDRAWAL_FEE_BPS,
+    )
 }
--- programs/pricing-programs/flat-fee/src/processor/set_lp_withdrawal_fee.rs
+++ programs/pricing-programs/flat-fee/src/processor/set_lp_withdrawal_fee.rs
@@ -1,9 +1,15 @@
 use flat_fee_interface::SetLpWithdrawalFeeIxArgs;
+use flat_fee_lib::processor::{
+    process_set_lp_withdrawal_fee_unchecked, verify_set_lp_withdrawal_fee,
+};
 use solana_program::{account_info::AccountInfo, entrypoint::ProgramResult};
 
 pub fn process_set_lp_withdrawal_fee(
-    _accounts: &[AccountInfo],
-    _args: SetLpWithdrawalFeeIxArgs,
+    accounts: &[AccountInfo],
+    SetLpWithdrawalFeeIxArgs {
+        lp_withdrawal_fee_bps,
+    }: SetLpWithdrawalFeeIxArgs,
 ) -> ProgramResult {
-    todo!()
+    let checked = verify_set_lp_withdrawal_fee(accounts)?;
+    process_set_lp_withdrawal_fee_unchecked(checked, lp_withdrawal_fee_bps)
 }
