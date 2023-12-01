e227b8e653b9f3f0b3027531779fbca4bda36058
Jesse Y. Cho <jessecho@cs.washington.edu>
2023-12-01 22:02:09 +0900

    impl set_lst_fee in prog

--- programs/pricing-programs/flat-fee/src/processor/set_lst_fee.rs
+++ programs/pricing-programs/flat-fee/src/processor/set_lst_fee.rs
@@ -1,6 +1,14 @@
 use flat_fee_interface::SetLstFeeIxArgs;
+use flat_fee_lib::processor::{process_set_lst_fee_unchecked, verify_set_lst_fee};
 use solana_program::{account_info::AccountInfo, entrypoint::ProgramResult};
 
-pub fn process_set_lst_fee(_accounts: &[AccountInfo], _args: SetLstFeeIxArgs) -> ProgramResult {
-    todo!()
+pub fn process_set_lst_fee(
+    accounts: &[AccountInfo],
+    SetLstFeeIxArgs {
+        input_fee_bps,
+        output_fee_bps,
+    }: SetLstFeeIxArgs,
+) -> ProgramResult {
+    let checked = verify_set_lst_fee(accounts)?;
+    process_set_lst_fee_unchecked(checked, input_fee_bps, output_fee_bps)
 }
