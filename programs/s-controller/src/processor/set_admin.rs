1a0d1befc90d2d72881d5f9f67b639b2daa12aa4
Johnnycus <sirjsx@gmail.com>
2023-12-05 12:56:57 +0600

    s-controller set-admin ix

--- programs/s-controller/src/processor/set_admin.rs
+++ programs/s-controller/src/processor/set_admin.rs
@@ -1,5 +1,47 @@
-use solana_program::{account_info::AccountInfo, entrypoint::ProgramResult};
+use s_controller_interface::{
+    set_admin_verify_account_keys, set_admin_verify_account_privileges, SetAdminAccounts,
+    SetAdminKeys,
+};
+use s_controller_lib::try_pool_state_mut;
+use sanctum_onchain_utils::utils::{
+    load_accounts, log_and_return_acc_privilege_err, log_and_return_wrong_acc_err,
+};
+use solana_program::{
+    account_info::AccountInfo, entrypoint::ProgramResult, program_error::ProgramError,
+};
 
-pub fn process_set_admin(_accounts: &[AccountInfo]) -> ProgramResult {
-    todo!()
+pub fn process_set_admin(accounts: &[AccountInfo]) -> ProgramResult {
+    let checked = verify_set_admin(accounts)?;
+    process_set_admin_unchecked(checked)
+}
+
+fn process_set_admin_unchecked(
+    SetAdminAccounts {
+        current_admin: _,
+        new_admin,
+        pool_state,
+    }: SetAdminAccounts,
+) -> ProgramResult {
+    let mut pool_state_data = pool_state.try_borrow_mut_data()?;
+    let pool_state = try_pool_state_mut(&mut pool_state_data)?;
+    pool_state.admin = *new_admin.key;
+
+    Ok(())
+}
+
+fn verify_set_admin<'a, 'info>(
+    accounts: &'a [AccountInfo<'info>],
+) -> Result<SetAdminAccounts<'a, 'info>, ProgramError> {
+    let actual: SetAdminAccounts = load_accounts(accounts)?;
+
+    let expected = SetAdminKeys {
+        current_admin: *actual.current_admin.key,
+        new_admin: *actual.new_admin.key,
+        pool_state: *actual.pool_state.key,
+    };
+
+    set_admin_verify_account_keys(&actual, &expected).map_err(log_and_return_wrong_acc_err)?;
+    set_admin_verify_account_privileges(&actual).map_err(log_and_return_acc_privilege_err)?;
+
+    Ok(actual)
 }
