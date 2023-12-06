2007ec1e2fb71d02923b0b065f82634573a865ef
Jesse Y. Cho <jessecho@cs.washington.edu>
2023-12-06 22:18:27 +0900

    add basic success and reject tests

---
+++ programs/s-controller/tests/disable_pool.rs
@@ -0,0 +1,121 @@
+mod common;
+
+use common::*;
+use s_controller_interface::{disable_pool_ix, DisablePoolIxArgs};
+use s_controller_lib::{
+    program::{DISABLE_POOL_AUTHORITY_LIST_ID, POOL_STATE_ID},
+    try_pool_state, DisablePoolFreeArgs, U8Bool,
+};
+use solana_program_test::{processor, ProgramTest};
+use solana_readonly_account::sdk::KeyedReadonlyAccount;
+use solana_sdk::{signature::Keypair, signer::Signer, transaction::Transaction};
+
+#[tokio::test]
+async fn basic_disable_pool() {
+    let mut program_test = ProgramTest::default();
+    program_test.add_program(
+        "s_controller",
+        s_controller_lib::program::ID,
+        processor!(s_controller::entrypoint::process_instruction),
+    );
+
+    let pool_state_account = pool_state_to_account(DEFAULT_POOL_STATE);
+    program_test.add_account(
+        s_controller_lib::program::POOL_STATE_ID,
+        pool_state_account.clone(),
+    );
+
+    let disable_pool_authority_kp = Keypair::new();
+    program_test = program_test_add_disable_pool_authority_list(
+        program_test,
+        &[disable_pool_authority_kp.pubkey()],
+    );
+
+    let (mut banks_client, payer, last_blockhash) = program_test.start().await;
+
+    let disable_pool_authority_list_acc =
+        banks_client_get_disable_pool_list_acc(&mut banks_client).await;
+
+    // Disable pool by disable pool authority
+    {
+        let keys = DisablePoolFreeArgs {
+            authority: disable_pool_authority_kp.pubkey(),
+            pool_state_acc: KeyedReadonlyAccount {
+                key: POOL_STATE_ID,
+                account: pool_state_account.clone(),
+            },
+            disable_pool_authority_list: KeyedReadonlyAccount {
+                key: DISABLE_POOL_AUTHORITY_LIST_ID,
+                account: disable_pool_authority_list_acc.clone(),
+            },
+        }
+        .resolve()
+        .unwrap();
+        let ix = disable_pool_ix(keys, DisablePoolIxArgs {}).unwrap();
+        let mut tx = Transaction::new_with_payer(&[ix], Some(&payer.pubkey()));
+        tx.sign(&[&payer, &disable_pool_authority_kp], last_blockhash);
+
+        banks_client.process_transaction(tx).await.unwrap();
+
+        let pool_state_acc = banks_client_get_pool_state_acc(&mut banks_client).await;
+        let pool_state = try_pool_state(&pool_state_acc.data).unwrap();
+
+        assert!(U8Bool(pool_state.is_disabled).is_true());
+    }
+}
+
+#[tokio::test]
+async fn reject_disable_pool() {
+    let mut program_test = ProgramTest::default();
+    program_test.add_program(
+        "s_controller",
+        s_controller_lib::program::ID,
+        processor!(s_controller::entrypoint::process_instruction),
+    );
+
+    let pool_state_account = pool_state_to_account(DEFAULT_POOL_STATE);
+    program_test.add_account(
+        s_controller_lib::program::POOL_STATE_ID,
+        pool_state_account.clone(),
+    );
+
+    let disable_pool_authority_kp = Keypair::new();
+    program_test = program_test_add_disable_pool_authority_list(
+        program_test,
+        &[disable_pool_authority_kp.pubkey()],
+    );
+
+    let (mut banks_client, payer, last_blockhash) = program_test.start().await;
+
+    let disable_pool_authority_list_acc =
+        banks_client_get_disable_pool_list_acc(&mut banks_client).await;
+
+    // Disable pool by disable pool authority
+    {
+        // A keypair not authorized to disable pool
+        let rando_kp = Keypair::new();
+        let keys = DisablePoolFreeArgs {
+            authority: disable_pool_authority_kp.pubkey(),
+            pool_state_acc: KeyedReadonlyAccount {
+                key: POOL_STATE_ID,
+                account: pool_state_account.clone(),
+            },
+            disable_pool_authority_list: KeyedReadonlyAccount {
+                key: DISABLE_POOL_AUTHORITY_LIST_ID,
+                account: disable_pool_authority_list_acc.clone(),
+            },
+        }
+        .resolve()
+        .unwrap();
+        let ix = disable_pool_ix(keys, DisablePoolIxArgs {}).unwrap();
+        let mut tx = Transaction::new_with_payer(&[ix], Some(&payer.pubkey()));
+        tx.sign(&[&payer, &rando_kp], last_blockhash);
+
+        banks_client.process_transaction(tx).await.unwrap();
+
+        let pool_state_acc = banks_client_get_pool_state_acc(&mut banks_client).await;
+        let pool_state = try_pool_state(&pool_state_acc.data).unwrap();
+
+        assert!(U8Bool(pool_state.is_disabled).is_false());
+    }
+}
