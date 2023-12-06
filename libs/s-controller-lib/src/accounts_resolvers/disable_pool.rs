4b60ee4c74badc2ca4be35c47624772bfb111fe4
Jesse Y. Cho <jessecho@cs.washington.edu>
2023-12-06 21:32:57 +0900

    account resolver to verify authority exists in disable pool authority list

--- libs/s-controller-lib/src/accounts_resolvers/disable_pool.rs
+++ libs/s-controller-lib/src/accounts_resolvers/disable_pool.rs
@@ -2,7 +2,10 @@ use s_controller_interface::{DisablePoolKeys, SControllerError};
 use solana_program::pubkey::Pubkey;
 use solana_readonly_account::{KeyedAccount, ReadonlyAccountData};
 
-use crate::program::{DISABLE_POOL_AUTHORITY_LIST_ID, POOL_STATE_ID};
+use crate::{
+    program::{DISABLE_POOL_AUTHORITY_LIST_ID, POOL_STATE_ID},
+    try_disable_pool_authority_list, try_find_element_in_list,
+};
 
 #[derive(Clone, Copy, Debug)]
 pub struct DisablePoolFreeArgs<
@@ -21,6 +24,11 @@ impl<S: ReadonlyAccountData + KeyedAccount, L: ReadonlyAccountData + KeyedAccoun
         if *self.pool_state_acc.key() != POOL_STATE_ID {
             return Err(SControllerError::IncorrectPoolState);
         }
+
+        let disable_pool_authority_list_data = self.disable_pool_authority_list.data();
+        let list = try_disable_pool_authority_list(&disable_pool_authority_list_data)?;
+        try_find_element_in_list(self.authority, list)
+            .ok_or(SControllerError::InvalidDisablePoolAuthority)?;
 
         Ok(DisablePoolKeys {
             authority: self.authority,
