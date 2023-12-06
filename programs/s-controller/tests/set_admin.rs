3a331718cd84eaaf09f5347656874e257bcbacf4
Johnnycus <sirjsx@gmail.com>
2023-12-06 16:33:59 +0600

    update set_admin test to update admin two times

--- programs/s-controller/tests/set_admin.rs
+++ programs/s-controller/tests/set_admin.rs
@@ -22,6 +22,7 @@ async fn basic_set_admin() {
         read_keypair_file(test_fixtures_dir().join("s-controller-test-initial-authority-key.json"))
             .unwrap();
     let new_admin_kp = Keypair::new();
+    let another_new_admin_kp = Keypair::new();
 
     let mut program_test = ProgramTest::default();
 
@@ -51,18 +52,19 @@ async fn basic_set_admin() {
         },
     );
 
-    let pool_state_account = pool_state_to_account(DEFAULT_POOL_STATE);
+    let pool_state_acc = pool_state_to_account(DEFAULT_POOL_STATE);
 
-    program_test.add_account(POOL_STATE_ID, pool_state_account.clone());
+    program_test.add_account(POOL_STATE_ID, pool_state_acc.clone());
 
     let (mut banks_client, payer, last_blockhash) = program_test.start().await;
 
+    // Change admin
     let ix = set_admin_ix(
         SetAdminFreeArgs {
             new_admin: new_admin_kp.pubkey(),
             pool_state: KeyedReadonlyAccount {
                 key: POOL_STATE_ID,
-                account: pool_state_account.clone(),
+                account: pool_state_acc.clone(),
             },
         }
         .resolve()
@@ -78,11 +80,40 @@ async fn basic_set_admin() {
 
     let pool_state_acc = banks_client_get_pool_state_acc(&mut banks_client).await;
     let pool_state = try_pool_state(&pool_state_acc.data).unwrap();
-
     assert_eq!(
         *pool_state,
         PoolState {
             admin: new_admin_kp.pubkey(),
+            ..*pool_state
+        }
+    );
+
+    // Change admin again
+    let ix2 = set_admin_ix(
+        SetAdminFreeArgs {
+            new_admin: another_new_admin_kp.pubkey(),
+            pool_state: KeyedReadonlyAccount {
+                key: POOL_STATE_ID,
+                account: pool_state_acc.clone(),
+            },
+        }
+        .resolve()
+        .unwrap(),
+        SetAdminIxArgs {},
+    )
+    .unwrap();
+
+    let mut tx2 = Transaction::new_with_payer(&[ix2], Some(&payer.pubkey()));
+    tx2.sign(&[&payer, &new_admin_kp], last_blockhash);
+
+    banks_client.process_transaction(tx2).await.unwrap();
+
+    let pool_state_acc = banks_client_get_pool_state_acc(&mut banks_client).await;
+    let pool_state = try_pool_state(&pool_state_acc.data).unwrap();
+    assert_eq!(
+        *pool_state,
+        PoolState {
+            admin: another_new_admin_kp.pubkey(),
             ..*pool_state
         }
     );
