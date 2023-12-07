575b6e7613b7d83452d7f05463a7febd4d157e25
billythedummy <dhy1996@live.com.sg>
2023-12-08 00:55:09 +0800

    add basic tests

--- programs/s-controller/tests/tests/set_rebalance_authority.rs
+++ programs/s-controller/tests/tests/set_rebalance_authority.rs
@@ -2,13 +2,11 @@ use s_controller_interface::{
     set_rebalance_authority_ix, SControllerError, SetRebalanceAuthorityIxArgs,
 };
 use s_controller_lib::{
-    program::POOL_STATE_ID, try_pool_state, KnownAuthoritySetRebalanceAuthorityFreeArgs,
-    SetRebalanceAuthorityFreeArgs,
+    try_pool_state, KnownAuthoritySetRebalanceAuthorityFreeArgs, SetRebalanceAuthorityFreeArgs,
 };
 use solana_program::pubkey::Pubkey;
-use solana_program_test::{processor, BanksClient, ProgramTest};
+use solana_program_test::BanksClient;
 use solana_sdk::{
-    account::Account,
     signature::{read_keypair_file, Keypair},
     signer::Signer,
     transaction::Transaction,
@@ -24,14 +22,13 @@ async fn admin_set() {
             .unwrap();
     let new_rebalance_authority = Pubkey::new_unique();
 
-    let pool_state_acc = pool_state_to_account(DEFAULT_POOL_STATE);
-    let program_test = naked_pool_state_program_test(pool_state_acc.clone());
+    let program_test = naked_pool_state_program_test(DEFAULT_POOL_STATE);
     let (mut banks_client, payer, last_blockhash) = program_test.start().await;
 
     let ix = set_rebalance_authority_ix(
         KnownAuthoritySetRebalanceAuthorityFreeArgs {
             new_rebalance_authority,
-            pool_state: pool_state_acc,
+            pool_state: pool_state_to_account(DEFAULT_POOL_STATE),
         }
         .resolve_pool_admin()
         .unwrap(),
@@ -54,14 +51,13 @@ async fn rebalance_authority_set() {
     let mut pool_state = DEFAULT_POOL_STATE;
     pool_state.rebalance_authority = current_rebalance_authority.pubkey();
 
-    let pool_state_acc = pool_state_to_account(pool_state);
-    let program_test = naked_pool_state_program_test(pool_state_acc.clone());
+    let program_test = naked_pool_state_program_test(pool_state);
     let (mut banks_client, payer, last_blockhash) = program_test.start().await;
 
     let ix = set_rebalance_authority_ix(
         KnownAuthoritySetRebalanceAuthorityFreeArgs {
             new_rebalance_authority,
-            pool_state: pool_state_acc,
+            pool_state: pool_state_to_account(pool_state),
         }
         .resolve_current_rebalance_authority()
         .unwrap(),
@@ -80,8 +76,7 @@ async fn rebalance_authority_set() {
 async fn unauthorized_signer() {
     let new_rebalance_authority = Pubkey::new_unique();
 
-    let pool_state_acc = pool_state_to_account(DEFAULT_POOL_STATE);
-    let program_test = naked_pool_state_program_test(pool_state_acc.clone());
+    let program_test = naked_pool_state_program_test(DEFAULT_POOL_STATE);
     let (mut banks_client, payer, last_blockhash) = program_test.start().await;
 
     let ix = set_rebalance_authority_ix(
@@ -102,17 +97,6 @@ async fn unauthorized_signer() {
         err,
         SControllerError::UnauthorizedSetRebalanceAuthoritySigner,
     );
-}
-
-fn naked_pool_state_program_test(pool_state_account: Account) -> ProgramTest {
-    let mut program_test = ProgramTest::default();
-    program_test.add_program(
-        "s_controller",
-        s_controller_lib::program::ID,
-        processor!(s_controller::entrypoint::process_instruction),
-    );
-    program_test.add_account(POOL_STATE_ID, pool_state_account);
-    program_test
 }
 
 async fn verify_new_rebalance_authority(
