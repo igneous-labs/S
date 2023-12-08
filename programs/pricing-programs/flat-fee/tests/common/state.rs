dfb4445298556cec3279dc0086baf01f61577418
Jesse Y. Cho <jessecho@cs.washington.edu>
2023-12-08 21:37:27 +0900

    add initialize test; move test boilerplate to add_lst test

--- programs/pricing-programs/flat-fee/tests/common/state.rs
+++ programs/pricing-programs/flat-fee/tests/common/state.rs
@@ -4,26 +4,14 @@ use flat_fee_lib::{
     program::STATE_SIZE,
     utils::try_program_state_mut,
 };
+use solana_program_test::BanksClient;
 use solana_sdk::account::Account;
-use test_utils::est_rent_exempt_lamports;
+use test_utils::{banks_client_get_account, est_rent_exempt_lamports};
 
 pub const DEFAULT_PROGRAM_STATE: ProgramState = ProgramState {
     manager: initial_manager::ID,
     lp_withdrawal_fee_bps: INITIAL_LP_WITHDRAWAL_FEE_BPS,
 };
-//     total_sol_value: 0,
-//     trading_protocol_fee_bps: 0,
-//     lp_protocol_fee_bps: 0,
-//     version: 0,
-//     is_disabled: 0,
-//     is_rebalancing: 0,
-//     padding: [0u8; 1],
-//     admin: initial_authority::ID,
-//     rebalance_authority: initial_authority::ID,
-//     protocol_fee_beneficiary: initial_authority::ID,
-//     pricing_program: DEFAULT_PRICING_PROGRAM,
-//     lp_token_mint: Pubkey::new_from_array([0u8; 32]),
-// };
 
 pub fn program_state_to_account(state: ProgramState) -> Account {
     let mut data = vec![0u8; STATE_SIZE];
@@ -36,4 +24,8 @@ pub fn program_state_to_account(state: ProgramState) -> Account {
         executable: false,
         rent_epoch: u64::MAX,
     }
+}
+
+pub async fn banks_client_get_program_state_acc(banks_client: &mut BanksClient) -> Account {
+    banks_client_get_account(banks_client, flat_fee_lib::program::STATE_ID).await
 }
--- programs/pricing-programs/flat-fee/tests/tests/initialize.rs
+++ programs/pricing-programs/flat-fee/tests/tests/initialize.rs
@@ -1,24 +1,45 @@
+use flat_fee_interface::{initialize_ix, InitializeIxArgs};
+use flat_fee_lib::{account_resolvers::InitializeFreeArgs, utils::try_program_state};
 use solana_program_test::{processor, ProgramTest};
+use solana_sdk::{signature::read_keypair_file, signer::Signer, transaction::Transaction};
+use test_utils::test_fixtures_dir;
 
 use crate::common::*;
 
 #[tokio::test]
 async fn basic() {
+    let mock_auth_kp =
+        read_keypair_file(test_fixtures_dir().join("flat-fee-test-initial-manager-key.json"))
+            .unwrap();
+
     let mut program_test = ProgramTest::default();
+
     program_test.add_program(
         "flat_fee",
         flat_fee_lib::program::ID,
         processor!(flat_fee::entrypoint::process_instruction),
     );
 
-    let program_state_account = program_state_to_account(DEFAULT_PROGRAM_STATE);
-    program_test.add_account(
-        flat_fee_lib::program::STATE_ID,
-        program_state_account.clone(),
-    );
-
     let (mut banks_client, payer, last_blockhash) = program_test.start().await;
 
-    // TODO: write test
-    {}
+    // initialize
+    {
+        let ix = initialize_ix(
+            InitializeFreeArgs {
+                payer: payer.pubkey(),
+            }
+            .resolve(),
+            InitializeIxArgs {},
+        )
+        .unwrap();
+
+        let mut tx = Transaction::new_with_payer(&[ix], Some(&payer.pubkey()));
+        tx.sign(&[&payer, &mock_auth_kp], last_blockhash);
+
+        banks_client.process_transaction(tx).await.unwrap();
+
+        let state_acc = banks_client_get_program_state_acc(&mut banks_client).await;
+        let state = try_program_state(&state_acc.data).unwrap();
+        assert_eq!(*state, DEFAULT_PROGRAM_STATE);
+    }
 }
