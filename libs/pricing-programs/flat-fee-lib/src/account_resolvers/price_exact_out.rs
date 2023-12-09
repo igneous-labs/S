4719d09e399080b418d66eab1bfb5050f16832b5
billythedummy <dhy1996@live.com.sg>
2023-12-09 15:52:59 +0800

    fix price-exact-out

--- libs/pricing-programs/flat-fee-lib/src/account_resolvers/price_exact_out.rs
+++ libs/pricing-programs/flat-fee-lib/src/account_resolvers/price_exact_out.rs
@@ -22,7 +22,7 @@ impl PriceExactOutFreeArgs {
         let (input_fee_acc, _bump) = input_find_pda_args.get_fee_account_address_and_bump_seed();
 
         let output_find_pda_args = FeeAccountFindPdaArgs {
-            lst_mint: self.input_lst_mint,
+            lst_mint: self.output_lst_mint,
         };
         let (output_fee_acc, _bump) = output_find_pda_args.get_fee_account_address_and_bump_seed();
 
