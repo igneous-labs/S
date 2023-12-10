141f80303c5edbebdaa990083ebd1fbf015e666a
Jesse Y. Cho <jesse@socean.fi>
2023-12-10 22:57:28 +0900

    add set_lp_withdrawal_fee unauthorized test

--- programs/pricing-programs/flat-fee/tests/tests/set_lp_withdrawal_fee.rs
+++ programs/pricing-programs/flat-fee/tests/tests/set_lp_withdrawal_fee.rs
@@ -1,10 +1,14 @@
-use flat_fee_interface::{set_lp_withdrawal_fee_ix, ProgramState, SetLpWithdrawalFeeIxArgs};
+use flat_fee_interface::{
+    set_lp_withdrawal_fee_ix, ProgramState, SetLpWithdrawalFeeIxArgs, SetLpWithdrawalFeeKeys,
+};
 use flat_fee_lib::{
     account_resolvers::SetLpWithdrawalFeeFreeArgs, program::STATE_ID, utils::try_program_state,
 };
 use flat_fee_test_utils::banks_client_get_flat_fee_program_state;
+use solana_program::program_error::ProgramError;
 use solana_readonly_account::sdk::KeyedReadonlyAccount;
 use solana_sdk::{signature::Keypair, signer::Signer, transaction::Transaction};
+use test_utils::assert_program_error;
 
 use crate::common::normal_program_test;
 
@@ -47,4 +51,42 @@ async fn set_lp_withdrawal_fee_basic() {
     let state = try_program_state(&state_acc.data).unwrap();
 
     assert_eq!(state.lp_withdrawal_fee_bps, NEW_LP_WITHDRAWAL_FEE_BPS);
+}
+
+#[tokio::test]
+async fn set_lp_withdrawal_fee_fail_unauthorized() {
+    const NEW_LP_WITHDRAWAL_FEE_BPS: u16 = 69;
+    let manager = Keypair::new();
+
+    let program_test = normal_program_test(
+        ProgramState {
+            manager: manager.pubkey(),
+            lp_withdrawal_fee_bps: Default::default(),
+        },
+        &[],
+    );
+    let (mut banks_client, payer, last_blockhash) = program_test.start().await;
+
+    let ix = set_lp_withdrawal_fee_ix(
+        SetLpWithdrawalFeeKeys {
+            manager: payer.pubkey(),
+            state: STATE_ID,
+        },
+        SetLpWithdrawalFeeIxArgs {
+            lp_withdrawal_fee_bps: NEW_LP_WITHDRAWAL_FEE_BPS,
+        },
+    )
+    .unwrap();
+
+    let mut tx = Transaction::new_with_payer(&[ix], Some(&payer.pubkey()));
+    tx.sign(&[&payer], last_blockhash);
+
+    let err = banks_client.process_transaction(tx).await.unwrap_err();
+
+    assert_program_error(err, ProgramError::InvalidArgument);
+
+    let state_acc = banks_client_get_flat_fee_program_state(&mut banks_client).await;
+    let state = try_program_state(&state_acc.data).unwrap();
+
+    assert_ne!(state.lp_withdrawal_fee_bps, NEW_LP_WITHDRAWAL_FEE_BPS);
 }
