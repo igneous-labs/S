e081f6e8b1859e7d0195aa3a808ca8559d3d1b40
billythedummy <dhy1996@live.com.sg>
2024-01-16 15:38:36 +0800

    bound fee_bps_i16 negative

--- libs/pricing-programs/flat-fee-lib/src/fee_bound.rs
+++ libs/pricing-programs/flat-fee-lib/src/fee_bound.rs
@@ -3,7 +3,7 @@ use flat_fee_interface::FlatFeeError;
 const MAX_FEE_BPS: i16 = 10_000;
 
 pub fn verify_signed_fee_bps_bound(fee_bps_i16: i16) -> Result<(), FlatFeeError> {
-    if MAX_FEE_BPS < fee_bps_i16 {
+    if !(-MAX_FEE_BPS..=MAX_FEE_BPS).contains(&fee_bps_i16) {
         return Err(FlatFeeError::SignedFeeOutOfBound);
     }
     Ok(())
