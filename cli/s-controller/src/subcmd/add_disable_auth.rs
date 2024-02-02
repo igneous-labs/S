e6ab13534635382715dffdd21a756ff630d5d8af
billythedummy <dhy1996@live.com.sg>
2024-02-02 17:37:38 +0800

    WIP impl done

---
+++ cli/s-controller/src/subcmd/add_disable_auth.rs
@@ -0,0 +1,89 @@
+use std::str::FromStr;
+
+use clap::{
+    builder::{StringValueParser, TypedValueParser},
+    Args,
+};
+use s_controller_interface::add_disable_pool_authority_ix_with_program_id;
+use s_controller_lib::{find_pool_state_address, try_pool_state, AddDisablePoolAuthorityFreeArgs};
+use sanctum_solana_cli_utils::{parse_signer, TxSendingNonblockingRpcClient};
+use solana_readonly_account::sdk::KeyedAccount;
+use solana_sdk::{
+    message::{v0::Message, VersionedMessage},
+    pubkey::Pubkey,
+    transaction::VersionedTransaction,
+};
+
+use crate::{common::verify_admin, rpc::fetch_pool_state};
+
+use super::Subcmd;
+
+#[derive(Args, Debug)]
+#[command(
+    long_about = "Add an authority allowed to disable the entire pool to the list of authorities"
+)]
+pub struct AddDisableAuthArgs {
+    #[arg(
+        long,
+        short,
+        help = "The pool's admin. Defaults to config wallet if not set."
+    )]
+    pub admin: Option<String>,
+
+    #[arg(
+        help = "The authority to add",
+        value_parser = StringValueParser::new().try_map(|s| Pubkey::from_str(&s)),
+    )]
+    pub new_authority: Pubkey,
+}
+
+impl AddDisableAuthArgs {
+    pub async fn run(args: crate::Args) {
+        let Self {
+            admin,
+            new_authority,
+        } = match args.subcmd {
+            Subcmd::AddDisableAuth(a) => a,
+            _ => unreachable!(),
+        };
+
+        let payer = args.config.signer();
+        let rpc = args.config.nonblocking_rpc_client();
+        let program_id = args.program;
+
+        let admin_signer = admin.map(|s| parse_signer(&s).unwrap());
+        let admin = admin_signer.as_ref().unwrap_or(&payer);
+
+        let pool_state_acc = fetch_pool_state(&rpc, program_id).await;
+
+        let pool_state = try_pool_state(&pool_state_acc.data).unwrap();
+        verify_admin(pool_state, admin.pubkey()).unwrap();
+
+        let ix = add_disable_pool_authority_ix_with_program_id(
+            program_id,
+            AddDisablePoolAuthorityFreeArgs {
+                payer: payer.pubkey(),
+                new_authority,
+                pool_state_acc: KeyedAccount {
+                    pubkey: find_pool_state_address(program_id).0,
+                    account: pool_state_acc,
+                },
+            }
+            .resolve_for_prog(program_id)
+            .unwrap(),
+        )
+        .unwrap();
+
+        let mut signers = vec![payer.as_ref(), admin.as_ref()];
+        signers.dedup();
+
+        let rbh = rpc.get_latest_blockhash().await.unwrap();
+        let tx = VersionedTransaction::try_new(
+            VersionedMessage::V0(Message::try_compile(&payer.pubkey(), &[ix], &[], rbh).unwrap()),
+            &signers,
+        )
+        .unwrap();
+
+        rpc.handle_tx(&tx, args.send_mode).await;
+    }
+}
--- cli/s-controller/src/subcmd/mod.rs
+++ cli/s-controller/src/subcmd/mod.rs
@@ -1,18 +1,21 @@
 use clap::Subcommand;
 
-use self::init::InitArgs;
+use self::{add_disable_auth::AddDisableAuthArgs, init::InitArgs};
 
+mod add_disable_auth;
 mod init;
 
 #[derive(Debug, Subcommand)]
 pub enum Subcmd {
     Init(InitArgs),
+    AddDisableAuth(AddDisableAuthArgs),
 }
 
 impl Subcmd {
     pub async fn run(args: crate::Args) {
         match args.subcmd {
             Self::Init(_) => InitArgs::run(args).await,
+            Self::AddDisableAuth(_) => todo!(),
         }
     }
 }
