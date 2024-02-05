7528eb73afe97744c3d9d8ab19b57a876b7f806b
Jesse Y. Cho <jessecho@cs.washington.edu>
2024-02-06 01:30:52 +0900

    assert len != 0 in add_disable_pool_authority_list; add test for disable pool authority

--- cli/s-controller/tests/tests/disable_pool.rs
+++ cli/s-controller/tests/tests/disable_pool.rs
@@ -5,8 +5,11 @@ use s_controller_test_utils::{
     DEFAULT_POOL_STATE,
 };
 use solana_program_test::{BanksClient, ProgramTest};
+use solana_sdk::{signature::Keypair, signer::Signer};
 
-use crate::common::{setup_with_init_auth_as_payer, SctrProgramTest, TestSctrCmd};
+use crate::common::{
+    setup_with_init_auth_as_payer, setup_with_payer, SctrProgramTest, TestSctrCmd,
+};
 
 async fn assert_pool_disabled(bc: &mut BanksClient) {
     let pool_state_acc = bc.get_pool_state_acc().await;
@@ -18,10 +21,25 @@ async fn assert_pool_disabled(bc: &mut BanksClient) {
 async fn disable_pool_success_payer_admin() {
     let pt = ProgramTest::default()
         .add_s_program()
-        .add_pool_state(DEFAULT_POOL_STATE)
-        .add_disable_pool_authority_list(&[]);
+        .add_pool_state(DEFAULT_POOL_STATE);
 
     let (mut cmd, _cfg, mut bc, _mock_auth_kp) = setup_with_init_auth_as_payer(pt).await;
+
+    cmd.cmd_disable_pool();
+    let exec_res = cmd.exec_b64_txs(&mut bc).await;
+    assert_all_txs_success_nonempty(&exec_res);
+    assert_pool_disabled(&mut bc).await;
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn disable_pool_success_payer_authority() {
+    let authority = Keypair::new();
+    let pt = ProgramTest::default()
+        .add_s_program()
+        .add_pool_state(DEFAULT_POOL_STATE)
+        .add_disable_pool_authority_list(&[authority.pubkey()]);
+
+    let (mut cmd, _cfg, mut bc, _mock_auth_kp) = setup_with_payer(pt, authority).await;
 
     cmd.cmd_disable_pool();
     let exec_res = cmd.exec_b64_txs(&mut bc).await;
