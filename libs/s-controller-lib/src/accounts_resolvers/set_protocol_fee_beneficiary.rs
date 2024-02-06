fdb92e48cbd6072199a2dfe4fd4dc9e645a581d1
Jesse Y. Cho <jessecho@cs.washington.edu>
2024-02-07 06:48:52 +0900

    refactor account_resolver

--- libs/s-controller-lib/src/accounts_resolvers/set_protocol_fee_beneficiary.rs
+++ libs/s-controller-lib/src/accounts_resolvers/set_protocol_fee_beneficiary.rs
@@ -5,38 +5,39 @@ use solana_readonly_account::{ReadonlyAccountData, ReadonlyAccountPubkey};
 use crate::{find_pool_state_address, program::POOL_STATE_ID, try_pool_state};
 
 #[derive(Clone, Copy, Debug)]
-pub struct SetProtocolFeeBeneficiaryFreeArgs<S: ReadonlyAccountData + ReadonlyAccountPubkey> {
+pub struct SetProtocolFeeBeneficiaryFreeArgs<S> {
     pub new_beneficiary: Pubkey,
     pub pool_state: S,
 }
 
 impl<S: ReadonlyAccountData + ReadonlyAccountPubkey> SetProtocolFeeBeneficiaryFreeArgs<S> {
     pub fn resolve(self) -> Result<SetProtocolFeeBeneficiaryKeys, SControllerError> {
-        self.resolve_inner(POOL_STATE_ID)
+        if *self.pool_state.pubkey() != POOL_STATE_ID {
+            return Err(SControllerError::IncorrectPoolState);
+        }
+        self.resolve_with_pool_state_id(POOL_STATE_ID)
     }
+}
 
+impl<S: ReadonlyAccountData> SetProtocolFeeBeneficiaryFreeArgs<S> {
     pub fn resolve_for_prog(
         self,
         program_id: Pubkey,
     ) -> Result<SetProtocolFeeBeneficiaryKeys, SControllerError> {
         let pool_state_id = find_pool_state_address(program_id).0;
-        self.resolve_inner(pool_state_id)
+        self.resolve_with_pool_state_id(pool_state_id)
     }
 
-    fn resolve_inner(
+    pub fn resolve_with_pool_state_id(
         self,
         pool_state_id: Pubkey,
     ) -> Result<SetProtocolFeeBeneficiaryKeys, SControllerError> {
         let SetProtocolFeeBeneficiaryFreeArgs {
             new_beneficiary,
-            pool_state: pool_state_acc,
+            pool_state,
         } = self;
 
-        if *pool_state_acc.pubkey() != pool_state_id {
-            return Err(SControllerError::IncorrectPoolState);
-        }
-
-        let pool_state_data = pool_state_acc.data();
+        let pool_state_data = pool_state.data();
         let pool_state = try_pool_state(&pool_state_data)?;
 
         Ok(SetProtocolFeeBeneficiaryKeys {
